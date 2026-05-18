87cb40e60cadbcd6dbbe00d299fc0904634d1f54
pedrux1717 <pedruzzi.luca.07@itisdalmine.edu.it>
2026-05-18 15:37:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const sections = document.querySelectorAll(".section");
-    const links = document.querySelectorAll(".navbar a");
+const links = document.querySelectorAll(".navbar a");
 
-    const observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            links.forEach(link => link.classList.remove("active"));
-
-            const activeLink = document.querySelector(
-              `.navbar a[href="#${entry.target.id}"]`
-            );
-
-            if (activeLink) activeLink.classList.add("active");
-          }
-        });
-      },
-      {
-        rootMargin: "-40% 0px -40% 0px",
-        threshold: 0
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        links.forEach(link => link.classList.remove("active"));
+        const activeLink = document.querySelector(
+          `.navbar a[href="#${entry.target.id}"]`
+        );
+        if (activeLink) activeLink.classList.add("active");
       }
-    );
+    });
+  },
+  { rootMargin: "-40% 0px -40% 0px", threshold: 0 }
+);
 
-    sections.forEach(section => observer.observe(section));
+sections.forEach(section => observer.observe(section));
